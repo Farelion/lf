2b678f58b254d60e8abc26f69631a862b0e50810
Marcin <mczownik@gmail.com>
2019-03-23 07:09:52 +0100

    overall changes, using lib mixitup now instead of own js

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
-console.log('ran');
+$(function() {
 
-document.addEventListener('click', function (e) {
-  var button = e.target;
-
-  if (button.getAttribute('data-reset') === 'true') {
-    // Reset the filters
-    var filter = button.getAttribute('data-filter');
-    resetFilter(filter);
-  } else {
-    // Filter the tag
-    var filter = button.getAttribute('data-filter');
-    var tag    = button.getAttribute('data-filter-tag');
-    filterTag(filter, tag);
-  }
-});
-
-// Filter tag
-function filterTag (filter, tag) {
-  var items = document.querySelectorAll('.' + filter + ' > div');
-
-  for (var i = 0; i < items.length; i++) {
-    var itemTags = items[i].getAttribute('data-tags');
-
-    // Catch case with no tags
-    if (itemTags != null) {
-      if (itemTags.indexOf(tag) < 0) {
-        items[i].setAttribute('data-toggle', 'off');
+  $(".items").mixItUp({
+    load: {
+      filter: ""
+    },
+    layout: {
+       display: 'block'
+   },
+    controls: {
+      toggleFilterButtons: true,
+      toggleLogic: "and"
+    },
+    callbacks: {
+      onMixEnd: function(state) {
+      /*  if ($('.items').height() > 1){
+          $(this).parent().css("visibility", "visible");
+          console.log($('.items').height())
+        }
+        else{
+          $(this).parent().css("visibility", "hidden");
+            console.log($('.items').height())
+        }*/
+        console.log(state.activeFilter);
       }
     }
-  }
-}
-
-// Reset filters
-function resetFilter (filter) {
-  var items = document.querySelectorAll('.' + filter + ' > div');
-
-  for (var i = 0; i < items.length; i++) {
-    items[i].setAttribute('data-toggle', 'on');
-  }
-}
-
-
-$(document).ready(function(){
-    $("button.atal-button").click(function(){
-        $("button.atalfilters-button").show();
-    });
-    $("button.atalfilters-button").click(function(){
-        $("button.atalfilters-button").hide();
-    });
+  });
 });
 
-console.log(items[0])
+
+
+
+
+/* dziwnie to dziala to onmix + visiblity nie robi tego za dobrze meh przekmnic dalej */
